7a3ab4c1373dacbe4cd58843458eed5b0936d4c4
Emmanuel-Tejeda <emmanuel_tejeda@uri.edu>
2022-11-25 21:37:49 -0700

    added web configurere for CORS Policy

--- src/main/java/com/example/events/EventsApplication.java
+++ src/main/java/com/example/events/EventsApplication.java
@@ -3,13 +3,14 @@ package com.example.events;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class EventsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EventsApplication.class, args);
+		
 	}
-
 
 
 }
---
+++ src/main/java/com/example/events/core/config/WebMvcConfig.java
@@ -0,0 +1,23 @@
+package com.example.events.core.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author Edward P. Legaspi | czetsuya@gmail.com
+ * @since
+ */
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+        .allowedMethods("*")
+        .allowedOrigins("http://localhost:4200/")
+        .allowedHeaders("*")
+        .allowCredentials(false)
+        .maxAge(-1);
+  }
+}
