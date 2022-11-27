96d5fbf049f2f5fd9d9c05352d7664895f1e5e92
Emmanuel-Tejeda <emmanuel_tejeda@uri.edu>
2022-11-27 12:45:29 -0700

    Documentation in the booking model,  and Get all bookings api made

--- src/main/java/com/example/events/controller/BookingController.java
+++ src/main/java/com/example/events/controller/BookingController.java
@@ -6,6 +6,7 @@ import com.example.events.model.BookingModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 @RestController
 public class BookingController {
@@ -13,15 +14,37 @@ public class BookingController {
     @Autowired
     BookingJpa bookingJpa;
 
-
+  /**
+   * Adds a new client into the database
+   * 
+   * @param sid - The id of the booking we are looking for
+   * @return The booking if found in the database
+   */
     @GetMapping("event/booking/{id}")
     public Optional<BookingModel> getBookingById(@PathVariable("id") Long sid){
       return bookingJpa.findById(sid);
     }
 
+    /**
+     * Returns all the bookings in database
+     * 
+     * @return All the bookings as a list of type BookingModel
+     */
+    @GetMapping("event/booking")
+    public List<BookingModel> getBookings(){
+      return bookingJpa.findAll();
+    }
+
+    /**
+     * 
+     * @param newBooking - New booking to be added to the database
+     * @return The new booking if added to the database succesfully
+     */
     @PostMapping("event/booking")
     public Optional<BookingModel> addBooking(@RequestBody BookingModel newbooking){
         bookingJpa.save(newbooking);
         return bookingJpa.findById(newbooking.getBookingId());
     }
+
+    
 }
