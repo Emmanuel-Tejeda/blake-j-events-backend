ffdd0984fcba1e4f52558581d1c997ee71929c1c
Emmanuel-Tejeda <emmanuel_tejeda@uri.edu>
2022-12-11 12:49:24 -0700

    Added a service layer that handles the data access, and changed the controller to use the service rather than the jpa directly

--- src/main/java/com/example/events/controller/BookingController.java
+++ src/main/java/com/example/events/controller/BookingController.java
@@ -1,52 +1,55 @@
 package com.example.events.controller;
 
 
-import com.example.events.dao.BookingJpa;
 import com.example.events.model.BookingModel;
+import com.example.events.service.BookingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
-import java.util.Optional;
+
 @RestController
 public class BookingController {
 
     @Autowired
-    BookingJpa bookingJpa;
+    BookingService bookingService;
 
   /**
    * Adds a new client into the database
    * 
    * @param sid - The id of the booking we are looking for
    * @return The booking if found in the database
+   * @StatusCode Returns a 200 status code connection established
    */
     @GetMapping("event/booking/{id}")
-    public Optional<BookingModel> getBookingById(@PathVariable("id") Long sid){
-      return bookingJpa.findById(sid);
+    public BookingModel getBookingById(@PathVariable("id") Long sid){
+
+        return bookingService.getBookingById(sid);
     }
 
     /**
      * Returns all the bookings in database
      * 
      * @return All the bookings as a list of type BookingModel
+     * @StatusCode Returns a 200 status code connection established
      */
     @GetMapping("event/bookings")
     public List<BookingModel> getBookings(){
-      return bookingJpa.findAll();
+      return bookingService.getBookings();
     }
 
     /**
      * 
      * @param newBooking - New booking to be added to the database
      * @return The new booking if added to the database succesfully
+     * @StatusCode Returns a status code of 201 object created
      */
     @PostMapping("event/booking")
     public ResponseEntity<BookingModel> addBooking(@Valid @RequestBody BookingModel newBooking){
-        bookingJpa.save(newBooking);
+        bookingService.saveBooking(newBooking);
         return new ResponseEntity<BookingModel>(newBooking, HttpStatus.CREATED);
     }
 
