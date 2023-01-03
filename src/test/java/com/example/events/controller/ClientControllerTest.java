31ce78d0d0ea089d0606ed97be670728374322bc
Emmanuel-Tejeda <emmanuel_tejeda@uri.edu>
2023-01-02 20:47:55 -0700

    Added Client. Tests/services/jpa/model

--- src/test/java/com/example/events/controller/ClientControllerTest.java
+++ src/test/java/com/example/events/controller/ClientControllerTest.java
@@ -2,12 +2,8 @@ package com.example.events.controller;
 
 
 
-import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-
-
 import com.example.events.model.ClientModel;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.*;
@@ -17,7 +13,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
@@ -33,11 +28,12 @@ class ClientControllerTest {
     private MockMvc mockMvc;
 
     /**
-     * Test the post mapping addBookings
+     * Test the post mapping addClients
      *
      * @StatusCode 201 created
      */
     @Test
+    @Order(1)
     public void addClient() throws Exception{
 
         mockMvc.perform(MockMvcRequestBuilders
@@ -51,19 +47,74 @@ class ClientControllerTest {
 
     }
 
-    void getClientById() {
+    /**
+     * Tests getClient and checks for a random Client for their id and cost
+     *
+     * @StatusCode 200 Ok
+     */
+    @Test
+    @Order(2)
+    public void getClients() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/event/client/")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("[0].clientId").value("1"))
+                .andExpect(MockMvcResultMatchers.jsonPath("[0].clientLastName").value("Tejeda"));
+
     }
 
+    /**
+     * Tests getClientById and checks object is created by checking for existing ID
+     *
+     * @StatusCode 200 Ok
+     */
     @Test
-    void getClients() {
+    @Order(3)
+    void getClientById() throws Exception{
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/event/client/{id}", 1)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.clientId").value(1));
     }
 
+    /**
+     * Test the update mapping in the Client controller
+     *
+     * @StatusCode 200 Ok
+     */
     @Test
-    void updateClient() {
+    @Order(4)
+    void updateClient() throws Exception {
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .put("/event/client/{id}", 1)
+                        .content(asJsonString(new ClientModel(1L, "Jessabella", "Baez", "401-470-3753","jessie@gmail.com")))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.clientFirstName").value("Jessabella"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.clientLastName").value("Baez"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.clientPhoneNumber").value("401-470-3753"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.clientEmail").value("jessie@gmail.com"));
+
     }
 
+    /**
+     * Test Delete mapping in the Client controller
+     *
+     * @StatusCode 204 No Content
+     */
     @Test
-    void deleteClient() {
+    @Order(5)
+    void deleteClient() throws Exception {
+
+        mockMvc.perform(MockMvcRequestBuilders.delete("/event/client/{id}", 1))
+                .andExpect(status().isNoContent());
     }
 
     /**
