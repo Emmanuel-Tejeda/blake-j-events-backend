5349def02705116fe289ff537261be458d9a4047
emmanueltejeda <emmanuel_tejeda@uri.edu>
2023-01-11 01:10:31 -0500

    Added event Jpa repository and event servies

--- src/main/java/com/example/events/dao/EventJpa.java
+++ src/main/java/com/example/events/dao/EventJpa.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface eventJpa extends JpaRepository<EventModel, Long> {
+public interface EventJpa extends JpaRepository<EventModel, Long> {
 }
---
+++ src/main/java/com/example/events/service/EventService.java
@@ -0,0 +1,79 @@
+package com.example.events.service;
+
+import com.example.events.dao.EventJpa;
+import com.example.events.model.EventModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EventService {
+
+    @Autowired
+    EventJpa eventJpa;
+
+    /**
+     * Queries the database for the EventModel with the matching ID
+     *
+     * @param id of the Event we want to get
+     * @return requested EventModel Object
+     */
+    public EventModel getEventById(Long id){
+        return eventJpa.findById(id).orElseThrow();
+    }
+
+    /**
+     * Gets a list of Events
+     *
+     * @return List of EventModel
+     */
+    public List<EventModel> getEvents(){
+        return eventJpa.findAll();
+    }
+
+    /**
+     * Saves the Event we add
+     *
+     * @param EventModel the Event we want to save
+     * @return The saved Event
+     */
+    public EventModel saveEvent(EventModel EventModel){
+
+        return eventJpa.save(EventModel);
+    }
+
+    /**
+     * Updates a Event selected by the ID
+     *
+     * @param newEventModel the Event the user just sent
+     * @param id of the Event that needs to be updated
+     * @return updated Event
+     */
+    public EventModel updateEvent(EventModel newEventModel, Long id){
+
+        EventModel oldEventModel = eventJpa.findById(id).orElseThrow();
+
+        oldEventModel.setDateofEvent(newEventModel.getDateofEvent());
+        oldEventModel.setTimeOfEvent(newEventModel.getTimeOfEvent());
+        oldEventModel.setTypeOfEvent(newEventModel.getTypeOfEvent());
+        oldEventModel.setBudget(newEventModel.getBudget());
+        oldEventModel.setCity(newEventModel.getCity());
+        oldEventModel.setHeadCount(newEventModel.getHeadCount());
+        oldEventModel.setDesignOrPlanning(newEventModel.getDesignOrPlanning());
+
+        eventJpa.save(oldEventModel);
+
+        return oldEventModel;
+
+    }
+
+    /**
+     * Deletes a Event by its associated id
+     *
+     * @param id of the Event we want to delete
+     */
+    public void deleteEvent(Long id){
+        eventJpa.deleteById(id);
+    }
+}
